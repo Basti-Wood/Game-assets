51282f89b8ba05bd1d34c124527845017859b192
RedDiamond <SebWoody@gmail.com>
2024-09-09 18:27:50 +0200

    drag and drop not working yet

---
+++ godot game code/scripts/drop_zone.gd
@@ -0,0 +1,13 @@
+extends Marker2D
+
+
+func _draw():
+	draw_circle(Vector2.ZERO, 100, Color.RED)
+
+func deselect():
+	modulate = Color.RED
+
+func select():
+	for c in get_tree().get_nodes_in_group("zone"):
+		c.deselect()
+	modulate = Color.BLACK
